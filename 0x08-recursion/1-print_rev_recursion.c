63fafa3afde573215940be780ec664ff6f5b4f3a
Emmanu589 <kimutaie979@gmail.com>
2023-07-05 03:09:54 -0700

    2-strlen_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_rev_recursion - Prints a string in reverse.
+ * _print_rev_recursion - Prints a string in reverse
  * @s: the string to reverse
  * Return: Nothing.
  */
