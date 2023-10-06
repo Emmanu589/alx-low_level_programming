1dc76c8875ec13fbb55d443679a919f7f86087ae
Emmanu589 <kimutaie979@gmail.com>
2023-10-05 17:30:51 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,17 +35,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 
 	}
-	
 	nptr = malloc(new_size);
-
 	if (nptr == NULL)
 		return (NULL);
-	
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		nptr[i] = ((char *) ptr)[i];
 	}
-	
 	free(ptr);
 	return (nptr);
 }
