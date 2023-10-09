a04bd2bd29da6431da6089535e59a2ce4c1153ba
Emmanu589 <kimutaie979@gmail.com>
2023-10-09 09:50:20 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,14 +20,3 @@ int _strlen(char *s)
        
         return (i);
 }      
-
-/**
- * _strcpy - copies the string pointed to by src* to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-        int len, i;
