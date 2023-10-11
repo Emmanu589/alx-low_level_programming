a9ddbd45546d75c7fc308c51709aa37f0d1900c4
Emmanu589 <kimutaie979@gmail.com>
2023-10-11 08:21:34 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - returs index
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to function
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i])
+		
+			action(array[i]);
+	}
+}
